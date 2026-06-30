2e08a837dfdb3f94073b4bc3fab564a67f00b323
codesby-raviteja <raviyellam555@gmail.com>
2026-06-30 15:24:29 +0530

    Resolve merge conflict

--- peering.tf
+++ peering.tf
@@ -1,16 +1,8 @@
 resource "aws_vpc_peering_connection" "peer" {
   count = var.is_peering_required ? 1 : 0
 
-<<<<<<< Updated upstream
-  # Existing VPC
-  peer_vpc_id = data.aws_vpc.peer.id
-
-  # New VPC created by this module
-  vpc_id = aws_vpc.main.id
-=======
   peer_vpc_id = data.aws_vpc.peer.id
   vpc_id      = aws_vpc.main.id
->>>>>>> Stashed changes
 
   auto_accept = true
 
